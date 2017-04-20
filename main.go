40374e45bf5041b644a53737aad5493bc905b1a1
szydell <marcin@szydelscy.pl>
2017-04-21 00:38:21 +0200

    gtmgo

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import "os"
 import "fmt"
 import "github.com/szydell/gogtm"
+import "github.com/szydell/mstools"
 
 func main() {
 	gtmGblDir := os.Getenv("gtmgbldir")
 	fmt.Println("gtmgbldir: " + gtmGblDir)
+	gtmDist := os.Getenv("gtm_dist")
+	fmt.Println("gtm_dist: " + gtmDist)
 	err := gogtm.Start()
+	mstools.ErrCheck(err)
 
 }
