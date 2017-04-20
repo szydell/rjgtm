581c7d16c91d6e86449d26fb97530768be7042b4
Marcin Szydelski <marcin@szydelscy.pl>
2017-04-20 23:17:53 +0200

    gogtm init

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import "os"
 import "fmt"
+import "github.com/szydell/gogtm"
 
 func main() {
 	gtmGblDir := os.Getenv("gtmgbldir")
 	fmt.Println("gtmgbldir: " + gtmGblDir)
+	err := gogtm.Start()
+
 }
