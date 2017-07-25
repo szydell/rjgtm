f70454ce58e651b911713b6431d20d1faa8eef66
Marcin Szydelski <marcin@szydelscy.pl>
2017-07-25 13:54:23 +0200

    remove M files

--- rjworker/launcher.sh
+++ rjworker/launcher.sh
@@ -2,19 +2,18 @@
 
 . /opt/fis/6.3-001A/gtmprofile
 export gtm_icu_version="5.0"
-export gtm_chset="UTF-8"
 export gtm_repl_instance="/home/szydell/.fis-gtm/V6.3-001A_x86_64/g/gtm.repl"
 export gtm_chset="UTF-8"
 export gtm_log="/tmp/fis-gtm/V6.3-001A_x86_64"
 export gtm_prompt="GTM>"
 export gtm_retention="42"
 export gtmgbldir="/home/szydell/.fis-gtm/V6.3-001A_x86_64/g/gtm.gld"
-export gtmroutines="/home/szydell/.fis-gtm/V6.3-001A_x86_64/o/utf8*(/home/szydell/.fis-gtm/V6.3-001A_x86_64/r /home/szydell/.fis-gtm/r) /opt/fis/6.3-001A/utf8/plugin/o/utf8(/opt/fis/6.3-001A/utf8/plugin/r) /opt/fis/6.3-001A/utf8/libgtmutil.so /opt/fis/6.3-001A/utf8 /home/szydell/code/src/github.com/szydell/rjgtm/m/"
+#export gtmroutines="/home/szydell/.fis-gtm/V6.3-001A_x86_64/o/utf8*(/home/szydell/.fis-gtm/V6.3-001A_x86_64/r /home/szydell/.fis-gtm/r) /opt/fis/6.3-001A/utf8/plugin/o/utf8(/opt/fis/6.3-001A/utf8/plugin/r) /opt/fis/6.3-001A/utf8/libgtmutil.so /opt/fis/6.3-001A/utf8 /home/szydell/code/src/github.com/szydell/rjgtm/m/"
 export gtmdir="/home/szydell/.fis-gtm"
 export gtm_tmp="/tmp/fis-gtm/V6.3-001A_x86_64"
 export gtm_dist="/opt/fis/6.3-001A/utf8"
-export GTMCI="/home/szydell/code/src/github.com/szydell/rjgtm/m/gtmacces.ci"
-# enable mumps ^C
+#export GTMCI="/home/szydell/code/src/github.com/szydell/rjgtm/m/gtmacces.ci"
+#enable mumps ^C
 #export gtm_nocenable=0
 
 export gtm_etrap='I 0=$ST W "Process terminated by: ",$ZS,! ZHALT 1'
