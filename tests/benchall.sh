9bda912363d604a58e9a89c6ba673c1a4b610176
Marcin Szydelski <marcin@szydelscy.pl>
2018-02-19 18:12:57 +0100

    bench

--- tests/benchall.sh
+++ tests/benchall.sh
@@ -8,11 +8,11 @@ echo "Siege write 100 urls * 200 users"
 siege -f siege_write_url.txt -ronce -c250 | grep -v "HTTP/1.1 200"
 
 echo "Siege read 100 url * 200 users"
-siege -f siege_read_url.txt -ronce -c250
+siege -f siege_read_url.txt -ronce -c250 | grep -v "HTTP/1.1 200"
 
 echo "Siege data 100 urls * 200 users"
 siege -f siege_data_url.txt -ronce -c250 | grep -v "HTTP/1.1 200"
 
 
-echo "Siege delete 100 urls * 200 users"
-siege -f siege_delete_url.txt -ronce -c250 | grep -v "HTTP/1.1 200"
+#echo "Siege delete 100 urls * 200 users"
+#siege -f siege_delete_url.txt -ronce -c250 | grep -v "HTTP/1.1 200"
